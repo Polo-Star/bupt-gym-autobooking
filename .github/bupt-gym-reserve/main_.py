b2b627dda13e62772ac2ccdb338ce6ce3919d7a3
cjs <55310652+cjs2019@users.noreply.github.com>
2021-04-07 01:18:23 +0800

    Update main_.py

--- .github/bupt-gym-reserve/main_.py
+++ .github/bupt-gym-reserve/main_.py
@@ -1,11 +1,12 @@
 
 import crontab
+import time
 
 cnt = 100
 n = 0
 
 while n < cnt:
     n = n + 1;
-    echo sleep 1;
+    time.sleep(2)
     echo python3 main.py -u ${{ secrets.BUPT_USERNAME }} -p ${{ secrets.BUPT_PASSWORD }};
     
