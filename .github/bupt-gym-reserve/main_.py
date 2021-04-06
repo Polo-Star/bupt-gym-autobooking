f23a9676b65ccd51518c6a8f7e877f56f344bf2f
cjs <55310652+cjs2019@users.noreply.github.com>
2021-04-07 01:34:10 +0800

    Update main_.py

--- .github/bupt-gym-reserve/main_.py
+++ .github/bupt-gym-reserve/main_.py
@@ -7,5 +7,5 @@ n = 0
 while n < cnt:
     n = n + 1;
     time.sleep(2)
-    os.system("python3 main.py -u ${{ secrets.BUPT_USERNAME }} -p ${{ secrets.BUPT_PASSWORD }};");
+    os.system("python3 main.py -u ${ secrets.BUPT_USERNAME } -p ${ secrets.BUPT_PASSWORD }; exit()");
     
